6cd59eb2a9156aaa93e05d82cbbdf8e57a29785d
Justin Schweigert <justin@schweigert.dev>
2021-10-31 16:17:58 -0500

    Add example URL to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from './index.module.scss';
 
+const url = `${window.location.protocol}//${window.location.hostname}/sand_head/badkittybob/miroit`;
+
 const Home: NextPage = () => {
   return (
     <main className={styles.page}>
@@ -12,6 +14,9 @@ const Home: NextPage = () => {
       <p>
         howdy! enter Twitch usernames in the URL separated by forward slashes to
         view multiple chats at once!
+      </p>
+      <p>
+        ex: <code className={styles.url}>{url}</code>
       </p>
       <p>
         <a
